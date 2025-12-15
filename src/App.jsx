9c900943e85eb91188f2f69e8068e4678b748c71
Eswar3321 <eswararao8786@gmail.com>
2025-12-15 21:33:01 +0530

    files structured

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import AllRoutes from './routes/AllRoutes.jsx';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <AllRoutes />
+    </div>
+  );
+}
+
+export default App;
